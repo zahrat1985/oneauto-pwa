50b02a0df86c710be1553feb17d37a4ad7679ea7
Zahra Taghizadeh Abbasabad <45653355+zahrat1985@users.noreply.github.com>
2026-01-23 13:38:07 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,1 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173
+  },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true
+  }
+});
 
